e9b06ecfcff272d8c36f459a2ec5bbe42e148473
umanorapracadadia <68360819+umanorapracadadia@users.noreply.github.com>
2022-04-22 14:06:13 -0500

    remove key

--- etherscan-block-function.py
+++ etherscan-block-function.py
@@ -19,7 +19,7 @@ from etherscan import Etherscan
 # In a python Lambda, the runtime loads the handler code as a module; so code in the top level
 # of the module occurs once, during cold start.
 print("Lambda Handler starting up")
-eth = Etherscan("Y3WUGXJMF8F755634XHTWAVIDA23IRHZWB") # key in quotation marks
+eth = Etherscan("XXXXXXXXXXXXXXX") # key in quotation marks
 
 # call made to access to blocks
 block_number = eth.get_block_number_by_timestamp(now, "before")
